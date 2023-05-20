2a5a0eb12e6376663c70089fab57b15eadfe82be
Paulina Matysiak <89973@student.ue.poznan.pl>
2023-05-21 00:43:14 +0200

    dodano trapez

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -32,3 +32,15 @@ r = 14
 obwod_kola = 2*math.pi*r
 pole_kola = math.pi*math.pow(r,2)
 print("Obwod kola wynosi: " + str(obwod_kola) + ", a pole wynosi " +str(pole_kola))
+
+
+#trapez
+a = 11
+b = 5
+c = 7
+d= 9
+h = 3
+
+pole_trapezu = ((a+b)*h)/2
+obwod_trapezu = a+b+c+d
+print("Obwod trapezu wynosi: " + str(obwod_trapezu) + ", a pole wynosi " +str(pole_trapezu))
